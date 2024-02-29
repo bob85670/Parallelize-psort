7d2b1d9b350570cd25afbee7bc2c3e483e9ff371
bob85670 <bob85670@connect.hku.hk>
2024-02-29 22:17:47 +0800

    Remove empty lines

--- psort_3035686516.c
+++ psort_3035686516.c
@@ -59,8 +59,6 @@ int index3 = 0;
 
 //int countdebug = 0;
 
-
-
 void *phrase1func(void *idp){
 
   int *my_id = (int *)idp;
@@ -78,32 +76,24 @@ void *phrase1func(void *idp){
   }
 
   //printf("numTakenEach is %d\n", numTakenEach);
-
   //printf("intarr[0]: %d", intarr[0]);
-
 
   // allocate memory for each "sub array", it is an array of pointers
   int *jagged[numTakenEach];
-
   for (int i = 0; i < numTakenEach; i++){
     jagged[i] = malloc(sizeof(int) * 1);
   }
 
-
   //printf("allocated size.");
-
 
   // amend sizeOf_temparr array
   sizeOf_temparr[(*my_id-1)] = numTakenEach;
-
   for(int i = 0; i < sizeof(sizeOf_temparr); i++){
     //printf("sizeof_temparr %d\n", sizeOf_temparr[i]);
   }
 
-
   // for index of intarr
   currentEle = numTakenEach * ((*my_id-1));
-
   if(*my_id == numOfWorker){
     currentEle = size - numTakenEach;
   }
@@ -116,56 +106,40 @@ void *phrase1func(void *idp){
   //printf("lastElementPtr's index is %d\n", currentEle + numTakenEach - 1);
   //printf("lastElementPtr is %d\n", *lastElementPtr);
 
-
-
   // initialize iterate pointer
   int *p;
   p = &intarr[currentEle];
   //printf("p is %d\n", *p);
 
-
-  
   // copy element in intarr[] to jagged[]
   int j = 0;
   while(1){
-
-
     if (p != lastElementPtr){
       *jagged[j] = intarr[currentEle+j];
-
-
       //printf("jagged[%d] = %d\n", j, *jagged[j]);
-
       j++;
       p++;
       //countdebug ++; 
 
-
     }else{
       *jagged[j] = intarr[currentEle+j];
       //printf("jagged[%d] = %d\n", j, *jagged[j]);
-
       //countdebug ++; 
       //printf("countdebug %d\n", countdebug);
-
       //printf("p arrives to the end.\n");
-
       j++;
       break;
     }
   }
 
-
   // qsort
   int qsortArray[j];
-
   //printf("j = %d\n", j);
   for (int i = 0; i < j; i++){
     qsortArray[i] = *jagged[i];
 
     //printf("qsortArray[%d] = %d\n", i, qsortArray[i]);
   }
-
 
   qsort(qsortArray, j, sizeof(int), compare);
 
@@ -174,8 +148,6 @@ void *phrase1func(void *idp){
     //printf("qsortArray[%d] after sorting = %d\n", i, qsortArray[i]);
   //}
 
-
-
   // store the subarray in Bigarray
   for (int i = 0; i < j; i++){
     Bigarray[(*my_id-1)][i] = qsortArray[i];
@@ -183,40 +155,27 @@ void *phrase1func(void *idp){
 
   }
 
-
-
   // each worker selects p samples from its 'local' sequence at corresponding indices and put into phase2array
   int x = 0;
   int currentIndex;
 
   for (int i = 0; i < numOfWorker; i++){
     currentIndex = (size * x)/ (numOfWorker * numOfWorker);
-
     //printf("currentIndex: %d\n", currentIndex);
 
     if (currentIndex < numTakenEach){
       phase2array[index2] = qsortArray[currentIndex];
-
       //printf("phase2array[%d] = %d\n", index2, phase2array[index2]);
     }
 
     index2 ++;
-
     x++;
   }
 
-
-  
   sem_post(&Sem1);
   //printf("Thread %d leaves semaphore!\n", *my_id);
-
-
   pthread_exit(NULL);
 }
-
-
-
-
 
 void *phrase3func(void *idp){
 
@@ -227,10 +186,8 @@ void *phrase3func(void *idp){
   //printf("\n");
   //printf("Thread %d arrives in semaphore 3!\n", *my_id);
 
-
   int phase4lengtharray[30];
   
-
   //partition according to phase3pivot, temparory store in phase4temparray
   int bigarrayIndex = 0;
   int rightIndex = 0;
@@ -243,17 +200,12 @@ void *phrase3func(void *idp){
 
   //printf("index3 = %d, size = %d\n", index3, size);
 
-
   while (count < (index3+size)){
     if (Bigarray[(*my_id-1)][bigarrayIndex] <= phase3pivot[rightIndex]){
-
       //printf("%d, pivot = %d\n", Bigarray[(*my_id-1)][bigarrayIndex], phase3pivot[rightIndex]);
 
-      
       phase4temparray[rightIndex][phase4Index] = Bigarray[(*my_id-1)][bigarrayIndex];
-
       //printf("phase4temparray[%d][%d] = %d\n", rightIndex, phase4Index, phase4temparray[rightIndex][phase4Index]);
-
 
       bigarrayIndex++;
       phase4Index++;
@@ -265,26 +217,21 @@ void *phrase3func(void *idp){
       sublength ++;
       //printf("sublength = %d\n", sublength);
 
-
     }else{
-
 
       rightIndex++;
       phase4Index = 0;
       count++;
 
       phase4lengtharray[rightIndex-1] = sublength;
-
       sublength = 0;
 
       //printf("count in else= %d\n", count);
-
       if (rightIndex == (index3-1)){
         break;
       }
     }
   }
-
 
   // last part of partition 
   //printf("rightIndex %d\n", rightIndex);
@@ -301,7 +248,6 @@ void *phrase3func(void *idp){
     phase4Index++;
 
     sublength++;
-
   }
 
   // mark down length of subarray
@@ -311,39 +257,26 @@ void *phrase3func(void *idp){
   //   printf("phase4lengtharray[%d] = %d\n", i,  phase4lengtharray[i]);
   // }
 
-
-
   //store back to phase4partitionarray
   for (int x = 0; x < index3; x++){
     for (int y = 0; y < phase4lengtharray[x]; y++){
-
-
       phase4partitionarray[x][phase4continueLengtharray[x]] = phase4temparray[x][y];
 
       //printf("phase4partitionarray[%d][%d] : %d\n", x, phase4continueLengtharray[x], phase4partitionarray[x][phase4continueLengtharray[x]]);
 
       phase4continueLengtharray[x] ++;
     }
-    
-
   }
 
   // for (int i = 0; i < index3; i++){
   //   printf("phase4continueLengtharray[%d] = %d\n", i, phase4continueLengtharray[i]);
   // }
 
-
-
   sem_post(&Sem3);
   //printf("Thread %d leaves semaphore!\n", *my_id);
 
-
   pthread_exit(NULL);
 }
-
-
-
-
 
 void *phrase5func(void *idp){
   int *my_id = (int *)idp;
@@ -353,26 +286,20 @@ void *phrase5func(void *idp){
   //printf("\n");
   //printf("Thread %d arrives in semaphore 5!\n", *my_id);
 
-
   //make phase5temparray[] a temporary 1D array
   int phase5temparray[phase4continueLengtharray[(*my_id-1)]];
-
   for (int i = 0; i < phase4continueLengtharray[(*my_id-1)]; i++){
     phase5temparray[i] = phase4partitionarray[(*my_id-1)][i];
 
     //printf("phase5temparray[%d] : %d\n", i, phase5temparray[i]);
   }
 
-
-
   //qsort
   qsort(phase5temparray, phase4continueLengtharray[(*my_id-1)], sizeof(int), compare);
-
   //debugging
   // for (int i = 0; i < phase4continueLengtharray[(*my_id-1)]; i++){
   //   printf("phase5temparray[%d] after sorting in Phase 5= %d\n", i, phase5temparray[i]);
   // }
-
 
   //put the sorted array into finalarray[],
   int startingpoint;
@@ -385,27 +312,19 @@ void *phrase5func(void *idp){
     startingpoint = phase4continueLengtharray[0];
     for (int i = 1; i < (*my_id-1); i++){
       startingpoint = startingpoint + phase4continueLengtharray[i]; 
-
     }
   }
 
   //printf("startingpoint : %d\n", startingpoint);
 
-
-
   for (int j = 0; j < phase4continueLengtharray[(*my_id-1)]; j++){
     finalarray[startingpoint+j] = phase5temparray[j];
 
     //printf("finalarray[%d] : %d\n", startingpoint+j, finalarray[startingpoint+j]);
-
   }
-
-
 
   sem_post(&Sem5);
   //printf("Thread %d leaves semaphore!\n", *my_id);
-
-
   pthread_exit(NULL);
 }
 
@@ -417,13 +336,10 @@ void *captainthreadfunc (void *arg){
   pthread_t threads[numOfWorker];
   sem_init(&Sem1, 0, 1);
 
-  
   //printf("Captain thread arrives.\n");
-
   //printf("intarr[0]: %d ,intarr[1]: %d , no. of item in intarr: %lu\n", intarr[0], *(intarr+1), size);
 
   //**************************************** Phase 1 **************************************** 
-
   for (int t = 0; t < numOfWorker; t++){
     int threadnum = t+1;
     pthread_create(&threads[t], NULL, phrase1func, (void*)&thread_ids[t]);
@@ -434,7 +350,6 @@ void *captainthreadfunc (void *arg){
   }
   //printf("Captain threads is back now.\n");
   //printf("\n");
-
 
   //**************************************** Phase 2 **************************************** 
 
@@ -448,9 +363,6 @@ void *captainthreadfunc (void *arg){
   //   printf("phase2array[%d] after sorting in Phase 2= %d\n", i, phase2array[i]);
   // }
 
-
-
-
   // Selects p-1 pivots from the phase2array
   int y = 1;
   int currentIndex2;
@@ -461,21 +373,16 @@ void *captainthreadfunc (void *arg){
 
     if ((currentIndex2 < sizeof(phase2array)) && phase2array[currentIndex2]!=0){
       phase3pivot[index3] = phase2array[currentIndex2];
-
       //printf("phase3pivot[%d] = %d\n", index3, phase3pivot[index3]);
-
     }
 
     index3 ++;
-
     y++;
   }
-
 
   //**************************************** Phase 3 & 4 **************************************** 
   
   //printf("Phase3 starts...\n");
-
 
   sem_init(&Sem3, 0, 1);
 
@@ -490,8 +397,6 @@ void *captainthreadfunc (void *arg){
   
   //printf("Captain threads is back now after Phase 4.\n");
   //printf("\n");
-
-
 
   //**************************************** Phase 5 **************************************** 
   
@@ -511,27 +416,18 @@ void *captainthreadfunc (void *arg){
   //printf("Captain threads is back now after Phase 5.\n");
   //printf("\n");
 
-
   //**************************************** After Phase 5 **************************************** 
   
   //printf("Print final array here ------------------------------ \n");
   for (int i = 0; i < size; i++){
     intarr[i] = finalarray[i];
 
-
     //printf("intarr[%d]: %d\n", i, intarr[i]);
   }
   //printf("Finish Printing final array ------------------------------ \n");
 
-  
-
-
   pthread_exit(NULL);
 }
-
-
-
-
 
 int main (int argc, char **argv)
 {
